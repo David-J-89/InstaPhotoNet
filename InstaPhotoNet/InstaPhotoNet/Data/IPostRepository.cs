cf8fecf56a7fcb4b4c673bdb483bc2b73a1d054a
David Whaley <davidwhaleycode@outlook.com>
2019-01-18 21:06:22 -0600

    created interface for postrepository

---
+++ InstaPhotoNet/InstaPhotoNet/Data/IPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using InstaPhotoNet.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InstaPhotoNet.Data
+{
+    public interface IPostRepository
+    {
+        void Add<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+
+        Task<bool> SaveAll();
+
+        Task<IEnumerable<Post>> GetPosts();
+
+        Task<User> GetUser(int id);
+
+        Task<Post> GetPost(int id);
+
+        Task<Post> GetProfilePhotoForUser(int userId);
+    }
+}
