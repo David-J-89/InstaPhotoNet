af5fb0ad01ffc634afbe2d29d3383c24ef4e8658
David Whaley <davidwhaleycode@outlook.com>
2019-01-18 20:36:44 -0600

    initial DataContext class

---
+++ InstaPhotoNet/InstaPhotoNet/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using InstaPhotoNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstaPhotoNet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
