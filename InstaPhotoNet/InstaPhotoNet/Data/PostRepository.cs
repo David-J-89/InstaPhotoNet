a0a5cfd43d2a67824bbee029b43213bf3a398990
David Whaley <davidwhaleycode@outlook.com>
2019-01-18 21:14:36 -0600

    created postrepository

---
+++ InstaPhotoNet/InstaPhotoNet/Data/PostRepository.cs
@@ -0,0 +1,60 @@
+﻿using InstaPhotoNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstaPhotoNet.Data
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly DataContext _context;
+
+        public PostRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<Post> GetPost(int id)
+        {
+            var post = await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
+
+            return post;
+        }
+
+        public async Task<IEnumerable<Post>> GetPosts()
+        {
+            var posts = await _context.Posts.OrderBy(post => post.DateAdded).ToListAsync();
+
+            return posts;
+        }
+
+        public async Task<Post> GetProfilePhotoForUser(int userId)
+        {
+            return await _context.Posts.Where(u => u.UserId == userId)
+                .FirstOrDefaultAsync(p => p.IsProfile);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _context.Users.Include(p => p.Posts).FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
