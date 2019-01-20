44dfa8ea3eac948a020fb58a89e0f612d28da06f
David Whaley <davidwhaleycode@outlook.com>
2019-01-19 18:26:29 -0600

    initial component setup

--- InstaPhotoNet/InstaPhotoNet/Client-SPA/src/app/app.module.ts
+++ InstaPhotoNet/InstaPhotoNet/Client-SPA/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { UsersComponent } from './users/users.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListsComponent,
+    UsersComponent,
+    MessagesComponent,
+    NavComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
